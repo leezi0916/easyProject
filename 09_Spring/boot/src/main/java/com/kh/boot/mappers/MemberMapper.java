58a811a310aec0d5a65dd81445e6c431c839dd51
leezi0916 <leezi0916@naver.com>
2025-03-11 13:35:58 +0900

    test8

---
+++ 09_Spring/boot/src/main/java/com/kh/boot/mappers/MemberMapper.java
@@ -0,0 +1,15 @@
+package com.kh.boot.mappers;
+
+import com.kh.boot.domain.vo.Member;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+/*
+@Mapper : MyBatis의 매퍼 인터페이스를 정의할 때 사용하는 어노테이션
+           스프링Bean으로 등록하여 의존성 주입이 가능하게 만들어 준다.
+ */
+@Mapper
+public interface MemberMapper {
+
+    Member loginMember(@Param("member") Member member);
+}
--- 09_Spring/boot/src/main/java/com/kh/boot/service/MemberServiceImpl.java
+++ 09_Spring/boot/src/main/java/com/kh/boot/service/MemberServiceImpl.java
@@ -1,13 +1,18 @@
 package com.kh.boot.service;
 
 import com.kh.boot.domain.vo.Member;
+import com.kh.boot.mappers.MemberMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service //@Component보다 더 구체화해서 service객체에 알맞게 bean에 등록
 public class MemberServiceImpl implements MemberService {
 
+    private final MemberMapper memberMapper;
+
     @Override
     public Member loginMember(Member m) {
-        return null;
+        return memberMapper.loginMember(m);
     }
 }
