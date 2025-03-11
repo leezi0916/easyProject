6658c05bac4b4d8ff8b69826ed167eb7ee3148f7
“leezi0916” <leezi0916@naver.com>
2025-03-11 09:56:20 +0900

    test4

--- 09_Spring/boot/src/main/java/com/kh/boot/ServletInitializer.java
+++ 09_Spring/boot/src/main/java/com/kh/boot/ServletInitializer.java
@@ -5,7 +5,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 /*
 	ServletInitializer : 스플링부트 어플리케이션을 배포할 때 설정을 지정하는 역할
-	war파일로 패키징하여 외부 톰캣등에 배포할 경우 필요함!!!!!!??????
+	war파일로 패키징하여 외부 톰캣등에 배포할 경우 필요함!!!!!!
  */
 public class ServletInitializer extends SpringBootServletInitializer {
 
