dc65be7760c829ad9022a7840000fa9d208c12e0
leezi0916 <leezi0916@naver.com>
2025-04-18 15:06:11 +0900

    no message

--- 10_React/react-project/src/App.jsx
+++ 10_React/react-project/src/App.jsx
@@ -1,16 +1,79 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import JavaScript from './components/JavaScript'
-import Heading from './components/Heading'
 import Hello from './components/Hello'
+import Heading from './components/Heading'
+import VideoList from './components/VideoList'
+
+const videoData = [{
+  sumbnail:"https://i.ytimg.com/an_webp/ugR9MOkqK_g/mqdefault_6s.webp?du=3000&sqp=CKi9h8AG&rs=AOn4CLCHATRFJmBeirLG2dsfaKqEhEvgGw",
+  title: "빵빵이와 옥지의 진솔한 대화(물리)",
+  logo: "https://yt3.ggpht.com/wYRkjS6E0mMZ-np2jNwjVaCNzQMpxs1VkdQ_p25oe0aaSj0awd7f9xRUcrwI6rVOQE7kjZQ6l4A=s48-c-k-c0x00ffffff-no-rj",
+  channelName: "빵빵이의 일상",
+  views: '8.3만',
+  date: "2시간 전",
+},{
+  sumbnail:"https://i.ytimg.com/an_webp/ugR9MOkqK_g/mqdefault_6s.webp?du=3000&sqp=CKi9h8AG&rs=AOn4CLCHATRFJmBeirLG2dsfaKqEhEvgGw",
+  title: "빵빵이와 옥지의 진솔한 대화(물리)",
+  logo: "https://yt3.ggpht.com/wYRkjS6E0mMZ-np2jNwjVaCNzQMpxs1VkdQ_p25oe0aaSj0awd7f9xRUcrwI6rVOQE7kjZQ6l4A=s48-c-k-c0x00ffffff-no-rj",
+  channelName: "빵빵이의 일상",
+  views: '8.3만',
+  date: "2시간 전",
+},{
+  sumbnail:"https://i.ytimg.com/an_webp/ugR9MOkqK_g/mqdefault_6s.webp?du=3000&sqp=CKi9h8AG&rs=AOn4CLCHATRFJmBeirLG2dsfaKqEhEvgGw",
+  title: "빵빵이와 옥지의 진솔한 대화(물리)",
+  logo: "https://yt3.ggpht.com/wYRkjS6E0mMZ-np2jNwjVaCNzQMpxs1VkdQ_p25oe0aaSj0awd7f9xRUcrwI6rVOQE7kjZQ6l4A=s48-c-k-c0x00ffffff-no-rj",
+  channelName: "빵빵이의 일상",
+  views: '8.3만',
+  date: "2시간 전",
+},{
+  sumbnail:"https://i.ytimg.com/an_webp/ugR9MOkqK_g/mqdefault_6s.webp?du=3000&sqp=CKi9h8AG&rs=AOn4CLCHATRFJmBeirLG2dsfaKqEhEvgGw",
+  title: "빵빵이와 옥지의 진솔한 대화(물리)",
+  logo: "https://yt3.ggpht.com/wYRkjS6E0mMZ-np2jNwjVaCNzQMpxs1VkdQ_p25oe0aaSj0awd7f9xRUcrwI6rVOQE7kjZQ6l4A=s48-c-k-c0x00ffffff-no-rj",
+  channelName: "빵빵이의 일상",
+  views: '8.3만',
+  date: "2시간 전",
+},{
+  sumbnail:"https://i.ytimg.com/an_webp/ugR9MOkqK_g/mqdefault_6s.webp?du=3000&sqp=CKi9h8AG&rs=AOn4CLCHATRFJmBeirLG2dsfaKqEhEvgGw",
+  title: "빵빵이와 옥지의 진솔한 대화(물리)",
+  logo: "https://yt3.ggpht.com/wYRkjS6E0mMZ-np2jNwjVaCNzQMpxs1VkdQ_p25oe0aaSj0awd7f9xRUcrwI6rVOQE7kjZQ6l4A=s48-c-k-c0x00ffffff-no-rj",
+  channelName: "빵빵이의 일상",
+  views: '8.3만',
+  date: "2시간 전",
+},{
+  sumbnail:"https://i.ytimg.com/an_webp/ugR9MOkqK_g/mqdefault_6s.webp?du=3000&sqp=CKi9h8AG&rs=AOn4CLCHATRFJmBeirLG2dsfaKqEhEvgGw",
+  title: "빵빵이와 옥지의 진솔한 대화(물리)",
+  logo: "https://yt3.ggpht.com/wYRkjS6E0mMZ-np2jNwjVaCNzQMpxs1VkdQ_p25oe0aaSj0awd7f9xRUcrwI6rVOQE7kjZQ6l4A=s48-c-k-c0x00ffffff-no-rj",
+  channelName: "빵빵이의 일상",
+  views: '8.3만',
+  date: "2시간 전",
+},{
+  sumbnail:"https://i.ytimg.com/an_webp/ugR9MOkqK_g/mqdefault_6s.webp?du=3000&sqp=CKi9h8AG&rs=AOn4CLCHATRFJmBeirLG2dsfaKqEhEvgGw",
+  title: "빵빵이와 옥지의 진솔한 대화(물리)",
+  logo: "https://yt3.ggpht.com/wYRkjS6E0mMZ-np2jNwjVaCNzQMpxs1VkdQ_p25oe0aaSj0awd7f9xRUcrwI6rVOQE7kjZQ6l4A=s48-c-k-c0x00ffffff-no-rj",
+  channelName: "빵빵이의 일상",
+  views: '8.3만',
+  date: "2시간 전",
+},{
+  sumbnail:"https://i.ytimg.com/an_webp/-lbqhtDyfe4/mqdefault_6s.webp?du=3000&sqp=CJ60h8AG&rs=AOn4CLAtde60hz-u8zCIWVaYD1tZRsB5fQ",
+  title: "빵빵이의 황금벚꽃",
+  logo: "https://yt3.ggpht.com/wYRkjS6E0mMZ-np2jNwjVaCNzQMpxs1VkdQ_p25oe0aaSj0awd7f9xRUcrwI6rVOQE7kjZQ6l4A=s48-c-k-c0x00ffffff-no-rj",
+  channelName: "빵빵이의 일상",
+  views: '2만',
+  date: "4시간 전",
+}]
+
 function App() {
   return (
     <>
-    <Heading type="h2"/>
-    <Heading/>
-    <Heading>
-      무엇을
-    </Heading>
-    <Hello/>
+      {/* <Heading type="h2"/>
+      <Heading/>
+      <Heading>
+        무엇을 도와드릴까요?
+      </Heading>
+      <Hello /> */}
+      <VideoList videos={videoData}/>
     </>
   )
 }
--- 10_React/react-project/src/components/Heading.jsx
+++ 10_React/react-project/src/components/Heading.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 
 const Heading = ({type, children}) => {
     
-    if(props.type === 'h2'){
-        return <h2>안녕하세요</h2>
+    if (type === "h2") {
+        return <h2>안녕하세요. Props입니다.</h2>
     }
+
     return (
         <>
-                <h1>안녕하세요</h1>
-                <h2>{props.children}</h2>
+            <h1>안녕하세요. Props입니다.</h1>
+            <h2>{children}</h2>
         </>
     )
 }
